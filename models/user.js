f7a5ff462dfbc4aa783e83124717b5913c2ee63d
yndx_geometrichno <klementevart@yandex.ru>
2023-12-19 18:05:27 +0500

    feat: add user db movel

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const bcrypt = require("bcrypt");
+const { Schema, model } = require("mongoose");
+const validator = require("validator");
+const ApiError = require('../error/ApiError')
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (v) => validator.isEmail(v),
+        message: (props) => `${props.value} - это некорректный Email`,
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    name: {
+      type: String,
+      required: true,
+      minlength: 2,
+      maxlength: 30,
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+//  eslint-disable-next-line func-names
+userSchema.statics.findUserByCredentials = async function (email, password) {
+  const user = await this.findOne({ email }).select("+password");
+  if (!user) {
+    return ApiError.unauthorized(`Неправильные почта или пароль`);
+  }
+  return bcrypt.compare(password, user.password).then((matched) => {
+    if (!matched) {
+      return ApiError.unauthorized(`Неправильные почта или пароль`);
+    }
+    return user;
+  });
+};
+
+module.exports = model("user", userSchema);
