3f83de071dbaea0e8f5a9ae4f225cf8c9c2adcb4
yndx_geometrichno <klementevart@yandex.ru>
2023-12-21 17:43:15 +0500

    main app file

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const { rateLimit } = require("express-rate-limit");
+const cors = require("cors");
+const helmet = require("helmet");
+const { errors } = require("celebrate");
+const { requestLogger, errorLogger } = require("./middleware/logger");
+const errorHandler = require("./middleware/ErrorHandlingMiddleWare");
+
+const router = require("./routes/index");
+const corsConfig = require("./utils/corsConfig");
+
+const DB_PORT = 3000;
+const DB_URL = "mongodb://127.0.0.1:27017/bitfilmsdb";
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 10000, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  message: "Too many requests from this IP, please try again later.",
+});
+
+const app = express();
+
+app.use(helmet());
+app.use(cors(corsConfig));
+app.use(limiter);
+app.use(express.json());
+app.use(cookieParser());
+app.use(requestLogger);
+
+mongoose.connect(DB_URL, {});
+
+app.use("/", router);
+app.use(errorLogger);
+app.use(errors());
+app.use(errorHandler);
+
+app.listen(DB_PORT);
