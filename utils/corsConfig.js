fec46697d050de8a165d2400d51a4e6ae16d1aaf
yndx_geometrichno <klementevart@yandex.ru>
2023-12-22 13:18:51 +0500

    Update corsConfig.js

--- utils/corsConfig.js
+++ utils/corsConfig.js
@@ -1,6 +1,8 @@
 const corsConfig = {
   origin: [
     "http://localhost:3000",
+    "http://api.movies.project.nomoredomainsmonster.ru/",
+    "https://api.movies.project.nomoredomainsmonster.ru/"
   ],
   credentials: true,
   maxAge: 30,
